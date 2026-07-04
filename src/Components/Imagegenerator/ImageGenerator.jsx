71a35cb9936e52865fea2b9307ae3ae9b2c8f56f
Abhishek Anand <anandabhi9619@gmail.com>
2026-07-04 13:27:25 +0530

    updated

--- src/Components/Imagegenerator/ImageGenerator.jsx
+++ src/Components/Imagegenerator/ImageGenerator.jsx
@@ -6,15 +6,21 @@ function ImageGenerator() {
 
     const   [image_url,setImage_url] = useState("/");
     let inputRef= useRef(null);
+    const [loading ,Setloading]=useState(false);
+
 
     const imageGenerator = async () => {
   if (inputRef.current.value === "") return;
+
+  Setloading(true); 
 
   const prompt = inputRef.current.value;
 
   const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`;
 
   setImage_url(imageUrl);
+
+  Setloading(false);
 };
     
 
@@ -23,9 +29,17 @@ function ImageGenerator() {
     <div className="header">AI Image <span>Generator</span></div>
     <div className="img-loading">
         <div className="image"><img src={image_url==="/"?default_img:image_url} alt='Not fixed'></img></div>
-    </div>
-<br></br>
+        <div className="loading">
+          <div className={loading ?"loading-bar-full":"loading-bar"}></div>
+          <div className={loading ?"loading-text":"display-none"}>Loading...</div>
+        </div>
 
+
+
+
+    </div>
+
+<br></br>
     <div className="search-box">
         <input type="text" ref={inputRef} className='search-input' placeholder='Describe your image to generate'    onKeyDown={(e) => {
     if (e.key === "Enter") {
